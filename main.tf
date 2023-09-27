4d4a757b03ea761e583c482b8f6306da474d1c53
Krishna <kritejaa@gmail.com>
2023-09-27 03:03:15 +0000

    #13 Migrate local state to terraform cloud using terraform cloud block

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
 terraform {
+              #  backend "remote" {
+              #    hostname = "app.terraform.io"
+              #    organization = "kriteja"
+
+              #    workspaces {
+              #      name = "terra-house-1"
+              #    }
+  cloud {
+    organization = "kriteja"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+  
   required_providers {
     random = {
       source = "hashicorp/random"
