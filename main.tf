734a23d76060367b6820fdf5051ca991754a8b0b
Krishna <40818088+kriteja@users.noreply.github.com>
2023-10-15 11:23:22 +0530

    41 terratowns provider (#44)
    * #41 wip setting up the skeleton for creating a custom terraform provider
    
    * #41 implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,14 +1,20 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   #backend "remote" {
   #  hostname = "app.terraform.io"
-  #  organization = "ExamPro"
+  #  organization = "kriteja"
 
   #  workspaces {
   #    name = "terra-house-1"
   #  }
   #}
   #cloud {
-  #  organization = "ExamPro"
+  #  organization = "kriteja"
   #  workspaces {
   #    name = "terra-house-1"
   #  }
@@ -16,12 +22,17 @@ terraform {
 
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="812b1b97-ef1d-4b8c-86c8-f211dafcaca2" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
+
+#module "terrahouse_aws" {
+#  source = "./modules/terrahouse_aws"
+#  user_uuid = var.user_uuid
+#  bucket_name = var.bucket_name
+#  index_html_filepath = var.index_html_filepath
+#  error_html_filepath = var.error_html_filepath
+#  content_version = var.content_version
+#}
